5061f56795b0aaff5a8be6a446ff7c05d4c1e7b6
JakeRoxs <jakemorgeson@live.com>
2026-03-16 21:28:45 -0500

    More attempt to de-vendor

--- Tools/generate_make_release.sh
+++ Tools/generate_make_release.sh
@@ -8,4 +8,8 @@ CMakeExePath="$ScriptPath"/Build/cmake/linux/bin/cmake
 echo "Generating $RootPath"
 echo "$CMakeExePath -S $RootPath -B $BuildPath"
 
-$CMakeExePath -S $RootPath -B $BuildPath -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release
+$CMakeExePath -S $RootPath -B $BuildPath -G "Unix Makefiles" \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DDSOS_USE_VENDORED_CIVETWEB=ON \
+  -DDSOS_USE_VENDORED_OPENSSL=ON \
+  -DDSOS_USE_VENDORED_SQLITE=ON
